5720ba35a19c3b7bf6223a567dd1b808527b31b9
Wojtek <grzesiek.kryty21@wp.pl>
2024-04-15 12:57:53 +0200

    Added News and Achievements

--- frontend/src/components/NewsCard.tsx
+++ frontend/src/components/NewsCard.tsx
@@ -11,32 +11,30 @@ import Image from 'next/image'
 import { CalendarPlus, User } from 'lucide-react'
 import Link from 'next/link'
 
-export default function NewsCard() {
+export default function NewsCard({author, src, date, opis, title, link} : {author: string, src: string, date: string, opis: string, title: string, link: string}) {
     return (
-        <Card className='w-96 h-96'>
-            <CardContent className='h-3/5 bg-neutral-200 rounded-t-lg'>
-
-            </CardContent>
+        <Card className='w-80 h-96 ml-8'>
+                <Image src={src} alt={'www'} width="0" height="0" sizes="100vw" className="w-full rounded-t-md max-h-56"></Image>
             <CardHeader className='py-2 px-2 '>
                 <CardTitle>
-                    <Link href={'#'} className='hover:underline'>
-                        Card Title
+                    <Link href={link} className='hover:underline'>
+                        {title}
 
                     </Link>
                 </CardTitle>
             </CardHeader>
             <CardContent className=' px-2 max-h-[20%] text-xs overflow-hidden relative'>
                 <div className='w-full h-full bg-gradient-to-b  from-transparent to-white absolute top-0 right-0 pointer-events-none' ></div>
-                <p className=' '>Dziś w Radiu Zielona Góra aż trzykrotnie zaprezentowano materiały o naszej szkole. Dotyczyły one zmiany na stanowisku dyrektora oraz utworzenia Branżowego Centrum Umiejętności.Dziś w Radiu Zielona Góra aż trzykrotnie zaprezentowano materiały o naszej szkole.</p>
+                <p className=' '>{opis}</p>
             </CardContent>
             <CardFooter className='py-2 px-2 flex justify-between  text-xs'>
                 <span className='flex gap-1 justify-center items-center'>
                     <CalendarPlus className='text-primary w-4 h-4'/>
-                    <p>4 września 2023, Poniedziałek</p>
+                    <p>{date}</p>
                 </span>
                 <span className='flex gap-1 justify-center items-center'>
                     <User className='text-primary w-4 h-4'/>
-                    <p>Mariusz Kmiećkowiak</p>
+                    <p>{author}</p>
                 </span>
             </CardFooter>
         </Card>
