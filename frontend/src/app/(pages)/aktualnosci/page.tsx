d6c88382788863cfe45e55db7107bd5f96236f56
Krzysztof Godyń - Perceptus <k.godyn@perceptus.pl>
2024-04-22 14:26:55 +0200

    resposive design article

--- frontend/src/app/(pages)/aktualnosci/page.tsx
+++ frontend/src/app/(pages)/aktualnosci/page.tsx
@@ -21,7 +21,10 @@ async function page() {
         title="Aktualności"
         subtitle="O to co dzieje się w naszej szkole!"
       />
-      <div className="grid w-full auto-rows-fr grid-cols-1 grid-rows-2 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <div  className="grid w-full  grid-cols-1  gap-4 md:grid-cols-2 lg:grid-cols-3  ">
+        
+
+        
         {
           <NewsCard
             key={articles[0].id}
@@ -55,47 +58,9 @@ async function page() {
                 />
               ))
           : "Brak artykułów do wyświetlenia"}
-      </div>
+        </div>
     </div>
   )
 }
 
 export default page
-
-function Test({ content, className }: any) {
-  return <div className={cn("bg-pink-500", className)}>{"Card " + content}</div>
-}
-
-// {
-//   <NewsCard
-//     key={articles[0].id}
-//     title={articles[0].attributes.title}
-//     author={articles[0].attributes.updatedBy}
-//     description={articles[0].attributes.description}
-//     link={`/aktualnosci/${articles[0].id}`}
-//     date={formatDate(articles[0].attributes.updatedAt)}
-//     src={
-//       "http://api.thefinalpath.net" +
-//       articles[0].attributes.image.data.attributes.url
-//     }
-//     className="!h-full !w-full [grid-column:span_3_/_span_3]"
-//   />
-// }
-// {articles.length
-//   ? articles
-//       .slice(1)
-//       .map((item: any, index: number) => (
-//         <NewsCard
-//           key={item.id}
-//           title={item.attributes.title}
-//           author={item.attributes.updatedBy}
-//           description={item.attributes.description}
-//           link={`/aktualnosci/${item.id}`}
-//           date={formatDate(item.attributes.updatedAt)}
-//           src={
-//             "http://api.thefinalpath.net" +
-//             item.attributes.image.data.attributes.url
-//           }
-//         />
-//       ))
-//   : "Brak artykułów do wyświetlenia"}
