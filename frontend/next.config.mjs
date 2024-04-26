93cf2c3f8ddb504e27728be6433583f233ec05da
Kacper Kozłowski <kati8ko@gmail.com>
2024-04-26 23:12:32 +0200

    fix: temp build fix

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: process.env.OUTPUT ? process.env.OUTPUT : undefined,
-  // experimental: {
-  //   missingSuspenseWithCSRBailout: false,
-  // },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   images: {
     remotePatterns: [
       {
