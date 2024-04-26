346bfc92b712ad21f8c02922a11ad1c45d76e4a6
Kacper Kozłowski <kati8ko@gmail.com>
2024-04-26 23:09:18 +0200

    chore: prepare for merge with auth

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: process.env.OUTPUT ? process.env.OUTPUT : undefined,
-  experimental: {
-    missingSuspenseWithCSRBailout: true,
-  },
   images: {
     remotePatterns: [
       {
