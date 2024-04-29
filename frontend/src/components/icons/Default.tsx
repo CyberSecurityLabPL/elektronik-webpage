cd4d72527f4df59ac16c459c72f89cae5bb9a2a8
carpye <kati8ko@gmail.com>
2024-04-29 11:49:08 +0200

    fix: kebab-case ->camelCase error

--- frontend/src/components/icons/Default.tsx
+++ frontend/src/components/icons/Default.tsx
@@ -12,16 +12,16 @@ function Default() {
       <path
         d="M20 2.66667H8.00001C7.29277 2.66667 6.61449 2.94762 6.11439 3.44772C5.61429 3.94781 5.33334 4.62609 5.33334 5.33333V26.6667C5.33334 27.3739 5.61429 28.0522 6.11439 28.5523C6.61449 29.0524 7.29277 29.3333 8.00001 29.3333H24C24.7073 29.3333 25.3855 29.0524 25.8856 28.5523C26.3857 28.0522 26.6667 27.3739 26.6667 26.6667V9.33333L20 2.66667Z"
         stroke="#808080"
-        stroke-width="3"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M18.6667 2.66667V8C18.6667 8.70725 18.9476 9.38552 19.4477 9.88562C19.9478 10.3857 20.6261 10.6667 21.3333 10.6667H26.6667"
         stroke="#808080"
-        stroke-width="3"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   )
