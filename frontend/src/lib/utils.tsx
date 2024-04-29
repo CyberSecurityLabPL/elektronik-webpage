013062fe1d5e1b27c5fd5664cb6ff37d7c5457c0
Pullolo <pllwot89@gmail.com>
2024-04-29 11:20:29 +0200

    random img in gallery link

--- frontend/src/lib/utils.tsx
+++ frontend/src/lib/utils.tsx
@@ -131,3 +131,9 @@ export const getAuthor = (data?: {
 
   return `${author.firstname} ${author.lastname}`
 }
+
+export function getRandomInt(min: number, max: number) {
+  const minCeiled = Math.ceil(min)
+  const maxFloored = Math.floor(max)
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled) // The maximum is exclusive and the minimum is inclusive
+}
