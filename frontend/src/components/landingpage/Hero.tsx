0b43e6a395f5f2971ddc7606e5294af861a2a965
Kacper Kozłowski <kati8ko@gmail.com>
2024-08-09 10:01:03 +0200

    (style): new hero look

--- frontend/src/components/landingpage/Hero.tsx
+++ frontend/src/components/landingpage/Hero.tsx
@@ -1,107 +1,122 @@
-"use client"
 import React from "react"
-import { Button } from "../ui/button"
+import { Button, buttonVariants } from "../ui/button"
 import Link from "next/link"
+import { cn } from "@/lib/utils"
+import { Lightbulb } from "lucide-react"
 import Image from "next/image"
-import { motion } from "framer-motion"
 
-export default function Hero({ data }: { data: any }) {
-  const heading = {
-    initial: {
-      y: -400,
-    },
-    animate: {
-      y: 0,
-      transition: {
-        delayChildren: 0.4,
-        staggerChildren: 0.04,
-        staggerDirection: -1,
-      },
-    },
-  }
-  const letter = {
-    initial: {
-      y: -400,
-    },
-    animate: {
-      y: 0,
-      transition: { duration: 1, ease: [0.6, 0.01, -0.05, 0.9] },
-    },
-  }
-
+const Hero2 = ({ data }: { data: any }) => {
   return (
-    <div>
-      <div className=" relative flex  w-full flex-col justify-center  gap-8 bg-hero-squares bg-no-repeat px-4  md:px-12 lg:flex-row xl:justify-around">
-        <div className="hero-content z-20 flex w-full items-center justify-around gap-8 pb-52 pt-12">
-          <div className=" flex flex-col items-center justify-center">
-            <motion.h1
-              className="relative  bg-gradient-to-r  from-primary to-primary-foreground bg-clip-text py-4  text-center text-4xl font-extrabold  text-transparent  lg:text-5xl xl:text-6xl"
-              initial={{
-                opacity: 0,
-                x: -500,
-              }}
-              animate={{
-                opacity: 1,
-                x: 0,
-              }}
-              transition={{
-                duration: 1.2,
-                delay: 0.2,
-              }}
-            >
-              {data?.heading ?? "Witaj w Elektroniku"}
-            </motion.h1>
-            <motion.p
-              className="max-w-[32rem] text-center text-base leading-relaxed text-primary-foreground md:text-lg"
-              initial={{
-                opacity: 0,
-                x: -200,
-              }}
-              animate={{
-                opacity: 1,
-                x: 0,
-              }}
-              transition={{
-                duration: 1.2,
-                delay: 0.4,
-              }}
-            >
-              {data?.text ?? ""}
-            </motion.p>
-            <span className="flex w-full justify-center py-4">
-              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-                <Button asChild variant={data?.link.type ?? ""}>
-                  <Link href={data?.link.link ?? "#overview"}>
-                    {data?.link.title ?? "Dowiedz się wiecej"}
-                  </Link>
-                </Button>
-              </motion.div>
-            </span>
-          </div>
-          <motion.div
-            className="hero-container relative z-30 hidden aspect-[125/84] w-[565px] rounded-3xl lg:block 2xl:w-[625px]"
-            initial={{
-              opacity: 0,
-              x: 500,
-            }}
-            animate={{
-              opacity: 1,
-              x: 0,
-            }}
-            transition={{
-              duration: 0.8,
-            }}
+    <div className="relative mb-20 flex w-full flex-col items-center px-4 py-20">
+      {/* BUBBLES BACKGROUND */}
+      <Image
+        src="/backgrounds/bubbles.svg"
+        alt=""
+        className="absolute bottom-0 left-0 -z-10 h-[848px] w-full"
+        width={1920}
+        height={1080}
+      />
+
+      <div className="relative flex w-full max-w-xl flex-col items-center gap-8 2xl:max-w-2xl">
+        <h1 className="text-center text-4xl font-bold text-primary xs:text-5xl xl:text-6xl">
+          {data?.heading ?? "Witaj w Elektroniku"}
+        </h1>
+        <p className="text-center text-base text-primary-foreground xs:text-lg xl:text-xl ">
+          {data?.text ?? "Zapraszamy do zapoznania się z naszą ofertą."}
+        </p>
+
+        {/* BUTTON */}
+
+        <div className="mt-20 flex flex-col gap-8 sm:flex-row">
+          <Link
+            href={"/articles"}
+            className={cn(
+              buttonVariants({ size: "lg" }),
+              "w-full max-w-xs border-[3px] border-primary py-6 text-lg"
+            )}
           >
-            <Image
-              alt="dyrektor"
-              src={process.env.NEXT_PUBLIC_BACKEND_URL + data.image.url}
-              fill
-              className="hero z-20 rounded-3xl object-cover "
-            />
-          </motion.div>
+            Sprawdź aktualności
+          </Link>
+          <Link
+            href={"#overview"}
+            className={cn(
+              buttonVariants({ variant: "outline", size: "lg" }),
+              "w-full max-w-xs py-6 text-lg"
+            )}
+          >
+            Dowiedz się więcej <Lightbulb className="ml-2 h-5 w-5" />
+          </Link>
         </div>
-        <div className="absolute bottom-0 left-0 z-10 h-48 w-full bg-lines-transition bg-repeat-x"></div>
+
+        {/* ARROWS */}
+
+        <Image
+          src="/assets/left-arrow.svg"
+          alt=""
+          className="absolute -left-[250px] top-0 hidden md:block"
+          width={200}
+          height={170}
+        />
+        <Image
+          src="/assets/right-arrow.svg"
+          alt=""
+          className="-inset-1rotate-12 absolute -right-[200px] top-[20px] hidden md:block"
+          width={200}
+          height={200}
+        />
+      </div>
+      <div className="mt-20 grid  w-full max-w-7xl grid-flow-dense grid-cols-2 grid-rows-1 gap-8  s:grid-rows-2 lg:grid-rows-1 lg:[grid-template-columns:_1fr_3fr_1fr]">
+        <Photo src="/assets/elektronik1.png" className="hidden s:block" />
+
+        <Photo
+          src="/assets/elektronik2.png"
+          className="-order-1 col-span-2 lg:col-span-1 lg:h-[400px]"
+        />
+        <Photo src="/assets/elektronik3.png" className="hidden s:block" />
       </div>
     </div>
   )
 }
+
+function Photo({ src, className }: { src: string; className?: string }) {
+  return (
+    <div
+      className={cn(
+        "relative h-[400px] rounded-3xl border-4 border-primary bg-primary/10 p-2",
+        className
+      )}
+    >
+      <div
+        className={
+          "h-full w-full rounded-2xl bg-cover bg-center bg-no-repeat  outline-[20px] outline-primary  "
+        }
+        style={{ backgroundImage: `url('${src}')` }}
+      />
+    </div>
+  )
+}
+
+export default Hero2
+
+// const heading = {
+//   initial: {
+//     y: -400,
+//   },
+//   animate: {
+//     y: 0,
+//     transition: {
+//       delayChildren: 0.4,
+//       staggerChildren: 0.04,
+//       staggerDirection: -1,
+//     },
+//   },
+// }
+// const letter = {
+//   initial: {
+//     y: -400,
+//   },
+//   animate: {
+//     y: 0,
+//     transition: { duration: 1, ease: [0.6, 0.01, -0.05, 0.9] },
+//   },
+// }
