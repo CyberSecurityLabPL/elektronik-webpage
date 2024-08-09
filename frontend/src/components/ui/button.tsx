fe605b45decec10899ea8e29eb6961a270be10c9
Kacper Kozłowski <kati8ko@gmail.com>
2024-08-09 05:13:10 +0200

    (style): bigger btn border

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border  border-primary text-primary bg-background hover:bg-primary hover:text-white",
+          "border-2 border-primary text-primary bg-background hover:bg-primary hover:text-white",
         secondary: "bg-secondary text-secondary-foreground hover:bg-slate-200",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
