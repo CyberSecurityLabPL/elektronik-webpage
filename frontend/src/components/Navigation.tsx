cbcd6c27fc50eea94377c54a705610d928a164a1
Krzysiek Godyn <Verti1234.krzysiek@o2.pl>
2024-08-25 00:36:25 +0200

    feat: add isFeatured badge to navItems

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -11,7 +11,7 @@ import { cn } from "@/lib/utils"
 import { ExternalLink } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
-import { Dispatch, forwardRef, SetStateAction } from "react"
+import { Dispatch, forwardRef, ReactNode, SetStateAction } from "react"
 import { Button } from "./ui/button"
 
 export function Navigation({
@@ -34,7 +34,7 @@ export function Navigation({
                   <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[800px]">
                     {index < 1 ? (
                       <li className="row-span-3">
-                        <NavigationMenuLink asChild >
+                        <NavigationMenuLink asChild>
                           <a
                             className={`relative flex select-none flex-col justify-end rounded-md bg-cover object-cover text-background no-underline outline-none focus:shadow-md `}
                             href="/galeria"
@@ -47,27 +47,30 @@ export function Navigation({
                               className="rounded-md object-cover"
                             />
 
-                          <div className="bg-white p-2 text-lg font-medium text-foreground ">
-                            Galeria
-                          </div>
-                          <p className="bg-white p-2 text-sm leading-tight text-foreground ">
-                            Zobacz naszą galerie szkolną gdzie znajdziesz
-                            przeróżne zdjęcia naszej szkoły! :)
-                          </p>
+                            <div className="bg-white p-2 text-lg font-medium text-foreground ">
+                              Galeria
+                            </div>
+                            <p className="bg-white p-2 text-sm leading-tight text-foreground ">
+                              Zobacz naszą galerie szkolną gdzie znajdziesz
+                              przeróżne zdjęcia naszej szkoły! :)
+                            </p>
                           </a>
                         </NavigationMenuLink>
                       </li>
                     ) : null}
-                    {item.links.map((tab: any) => (
-                      <ListItem
-                        key={tab.name}
-                        title={tab.name}
-                        href={tab.isExternal ? tab.href : `/${tab.href}`}
-                        target={tab.isExternal ? "_blank" : "_self"}
-                      >
-                        {tab?.description}
-                      </ListItem>
-                    ))}
+                    {item.links
+                      .sort((a: any, b: any) => b.isFeatured - a.isFeatured)
+                      .map((tab: any) => (
+                        <ListItem
+                          key={tab.name}
+                          title={tab.name}
+                          href={tab.isExternal ? tab.href : `/${tab.href}`}
+                          target={tab.isExternal ? "_blank" : "_self"}
+                          isFeatured={tab.isFeatured}
+                        >
+                          {tab?.description}
+                        </ListItem>
+                      ))}
                   </ul>
                 </NavigationMenuContent>
               </NavigationMenuItem>
@@ -105,8 +108,8 @@ export function Navigation({
 
 const ListItem = forwardRef<
   React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, href, ...props }, ref) => {
+  React.ComponentPropsWithoutRef<"a"> & { isFeatured?: boolean }
+>(({ className, title, children, href, isFeatured, ...props }, ref) => {
   const isExternal = href!.startsWith("http")
   return (
     <li>
@@ -121,7 +124,16 @@ const ListItem = forwardRef<
           {...props}
         >
           <div>
-            <div className="pb-2 text-sm font-medium leading-none">{title}</div>
+            <div className="flex w-full items-center  gap-3 pb-2 text-sm font-medium  leading-none">
+              {title}
+              {isFeatured ? (
+                <div className="rounded-3xl border border-primary/90 bg-primary/80 p-1 px-2 text-[12px]  text-white">
+                  Nowe
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
             <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
               {children}
             </p>
