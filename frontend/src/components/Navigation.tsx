20d063a8f4bc9fb5e12ab2b0249d5c068671d46f
Krzysiek Godyn <Verti1234.krzysiek@o2.pl>
2024-08-26 10:15:52 +0200

    fix: better UX navigation

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -8,30 +8,48 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 import { cn } from "@/lib/utils"
-import { ExternalLink } from "lucide-react"
+import { ChevronDown, ExternalLink } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
-import { Dispatch, forwardRef, ReactNode, SetStateAction } from "react"
+import { forwardRef, useEffect, useState, useRef } from "react"
 import { Button } from "./ui/button"
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover"
+import { usePathname } from "next/navigation"
+import { motion } from "framer-motion"
 
-export function Navigation({
-  navItems,
-  setIsExpanded,
-}: {
-  navItems: any
-  setIsExpanded: Dispatch<SetStateAction<boolean>>
-}) {
+export function Navigation({ navItems }: { navItems: any }) {
+  const pathname = usePathname()
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    setIsOpen(false)
+  }, [pathname])
+
+  const handlePopoverOpenChange = (open: boolean) => {
+    setIsOpen(open)
+  }
+
+  const handleMouseEnter = () => {
+    setIsOpen(false)
+  }
+
   return (
-    <NavigationMenu className="">
-      <NavigationMenuList className="">
+    <NavigationMenu>
+      <NavigationMenuList>
         {navItems
           ? navItems.slice(0, 4).map((item: any, index: number) => (
               <NavigationMenuItem key={item.name}>
-                <NavigationMenuTrigger className="rounded-xl bg-transparent text-lg hover:bg-transparent group-[[data-smaller=true]]:text-base">
+                <NavigationMenuTrigger
+                  onMouseEnter={handleMouseEnter}
+                  className="rounded-xl bg-transparent text-lg hover:bg-transparent group-[[data-smaller=true]]:text-base"
+                >
                   {item.name}
                 </NavigationMenuTrigger>
-                <NavigationMenuContent className="overflow-hidden ">
-                  <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[800px]">
+                <NavigationMenuContent className="overflow-hidden">
+                  <ul
+                    id="xD"
+                    className="relative !z-[99999] grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[800px]"
+                  >
                     {index < 1 ? (
                       <li className="row-span-3">
                         <NavigationMenuLink asChild>
@@ -46,11 +64,10 @@ export function Navigation({
                               height={100}
                               className="rounded-md object-cover"
                             />
-
-                            <div className="bg-white p-2 text-lg font-medium text-foreground ">
+                            <div className="bg-white p-2 text-lg font-medium text-foreground">
                               Galeria
                             </div>
-                            <p className="bg-white p-2 text-sm leading-tight text-foreground ">
+                            <p className="bg-white p-2 text-sm leading-tight text-foreground">
                               Zobacz naszą galerie szkolną gdzie znajdziesz
                               przeróżne zdjęcia naszej szkoły! :)
                             </p>
@@ -91,13 +108,65 @@ export function Navigation({
               </Link>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <Button
-                variant={"ghost"}
-                onClick={() => setIsExpanded((prev) => !prev)}
-                className="text-lg"
-              >
-                Więcej
-              </Button>
+              <Popover open={isOpen} onOpenChange={handlePopoverOpenChange}>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant={"ghost"}
+                    className="text-lg"
+                    onClick={() => handlePopoverOpenChange(!isOpen)}
+                  >
+                    Więcej
+                  </Button>
+                </PopoverTrigger>
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.25, ease: "easeInOut" }}
+                  className="z-10"
+                >
+                  <PopoverContent
+                    className="absolute -right-32 z-10 flex w-[52rem] rounded-3xl py-6 data-[state=open]:block data-[state=closed]:hidden"
+                    data-state={isOpen ? "open" : "closed"}
+                  >
+                    <ul className="mx-auto flex h-full w-full flex-wrap gap-8">
+                      {navItems.slice(4)?.map((item: any) => (
+                        <li key={item.name}>
+                          <Popover>
+                            <PopoverTrigger asChild>
+                              <Button
+                                variant={"secondary"}
+                                className={cn(
+                                  "text-lg font-medium text-slate-800"
+                                )}
+                              >
+                                {item.name}
+                                <ChevronDown className="ml-2" />
+                              </Button>
+                            </PopoverTrigger>
+                            <PopoverContent className="space-y-4 rounded-3xl">
+                              {item.links?.map((tab: any) => (
+                                <Link
+                                  key={tab.name}
+                                  className="flex gap-4 rounded-xl bg-slate-50 p-2 text-lg font-medium text-slate-800 hover:bg-slate-100 hover:text-slate-900"
+                                  href={
+                                    tab.isExternal ? tab.href : `/${tab.href}`
+                                  }
+                                  target={tab.isExternal ? "_blank" : "_self"}
+                                >
+                                  {tab.name}
+                                  {tab.isExternal && (
+                                    <ExternalLink className="h-6 w-6 text-muted-foreground" />
+                                  )}
+                                </Link>
+                              ))}
+                            </PopoverContent>
+                          </Popover>
+                        </li>
+                      ))}
+                    </ul>
+                  </PopoverContent>
+                </motion.div>
+              </Popover>
             </NavigationMenuItem>
           </>
         ) : null}
@@ -117,17 +186,17 @@ const ListItem = forwardRef<
         <Link
           ref={ref}
           className={cn(
-            "flex h-full select-none items-center justify-between gap-1 space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground ",
+            "flex h-full select-none items-center justify-between gap-1 space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
             className
           )}
           href={href!}
           {...props}
         >
           <div>
-            <div className="flex w-full items-center  gap-3 pb-2 text-sm font-medium  leading-none">
+            <div className="flex w-full items-center gap-3 pb-2 text-sm font-medium leading-none">
               {title}
               {isFeatured ? (
-                <div className="rounded-3xl border border-primary/90 bg-primary/80 p-1 px-2 text-[12px]  text-white">
+                <div className="rounded-3xl border border-primary/90 bg-primary/80 p-1 px-2 text-[12px] text-white">
                   Nowe
                 </div>
               ) : (
@@ -138,7 +207,7 @@ const ListItem = forwardRef<
               {children}
             </p>
           </div>
-          <div className="">
+          <div>
             {isExternal && (
               <ExternalLink className="h-6 w-6 text-muted-foreground" />
             )}
