3e0cac94b15c6c77a37619b1a03a328e209f0689
Verti1234 <krzysztofgodyn501@gmail.com>
2025-04-18 18:21:49 +0200

    strapi: modified articles

--- strapi/src/api/article/middlewares/article-populate-creator-fields.ts
+++ strapi/src/api/article/middlewares/article-populate-creator-fields.ts
@@ -2,6 +2,7 @@
 
 module.exports = (config, { strapi }) => {
   return async (ctx, next) => {
+    strapi.log.info(JSON.stringify(ctx.query.populate))
     if (!ctx.query.populate) {
       ctx.query.populate = ["createdBy", "updatedBy"];
     }
