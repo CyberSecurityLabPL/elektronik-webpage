59748d107cf278b723053adb7b16b1f1454e9c47
PFilip <2008filip@wp.pl>
2026-03-05 19:03:54 +0100

    feat(strapi): add email sending support

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -2,4 +2,16 @@ export default ({ env }) => ({
   "file-system": {
     enabled: true,
   },
+  email: {
+    config: {
+      provider: 'sendmail',
+      providerOptions: {
+        smtpHost: env('SMTPHOST', '172.18.0.1'),
+      },
+      settings: {
+        defaultFrom: env('EMAIL_DEFAULT_FROM', ''),
+        defaultReplyTo: env('EMAIL_DEFAULT_REPLY_TO', ''),
+      },
+    },
+  },
 });
