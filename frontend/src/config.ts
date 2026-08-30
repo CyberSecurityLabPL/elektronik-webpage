57f4240a72dc68808deef696071a49c1ba643d7c
PFilip <2008filip@wp.pl>
2026-08-30 21:49:19 +0200

    fix(timetable):

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -3,7 +3,7 @@ export const PAGINATION_LIMIT = 6
 export const REVALIDATE = 10
 
 export const timetableConfig = {
-  initialId: "o25",
+  initialId: "o19",
   formattingExceptions: [
     {
       initialValue: "witr. intern",
