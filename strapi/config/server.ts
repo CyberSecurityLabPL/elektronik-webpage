aae9e416c219a28d70e78a031a6de5900ddc9321
PFilip <2008filip@wp.pl>
2026-08-30 21:15:30 +0200

    chore: idk, it was on prod and was working

--- strapi/config/server.ts
+++ strapi/config/server.ts
@@ -4,6 +4,8 @@ export default ({ env }) => {
   return {
     host: env("HOST", "0.0.0.0"),
     port: env.int("PORT", 1337),
+    url: "https://api.elektronik.zgora.pl",
+    proxy: true,
     app: {
       keys: env.array("APP_KEYS"),
     },
